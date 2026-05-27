c5a7b332e599dce3edcc031b15e84888e3050c2f
Nebiyu <nebiyumekonnen17@gmail.com>
2026-05-27 13:05:22 -0700

    list.py created

--- lists.py
+++ lists.py
@@ -2,6 +2,12 @@
 colors = ["red", "blue", "green"]
 mixed = ["apple", 10, 3.5, True]
 
+colors[1] = "purple" # Rename
+
+colors.append("yellow") # Add a new item to the colors list
+
+mixed.append("pizza") # Add a new item to the mixed list
+
 # Print the lists
 print("Mixed list:", mixed)
 print("Colors list:", colors)
