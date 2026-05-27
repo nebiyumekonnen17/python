3ff2a09862d904745ecad8b1fd8cf5397544da59
Nebiyu <nebiyumekonnen17@gmail.com>
2026-05-27 12:31:08 -0700

    conditionals.py created

---
+++ conditionals.py
@@ -0,0 +1,8 @@
+bananas = 3 
+
+if bananas >= 5:
+    print("I have a bunch of bananas.")
+elif 1 <= bananas <= 4:
+    print("I have a small bunch of bananas.")
+else:
+    print("I have no bananas.")
